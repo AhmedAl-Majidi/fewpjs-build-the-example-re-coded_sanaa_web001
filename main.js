c9f8fa271c4eac6228c9dcc5ee5cdb91b4c84cef
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-21 06:34:19 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ let red = {
   "red" : ""
 };
 
-let articles = document.querySelectorAll('like');
+let articles = document.querySelectorAll('.like');
 
 function likeFn(e) {
   let heart = e.target;
